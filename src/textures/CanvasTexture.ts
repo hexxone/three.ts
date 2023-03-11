e1f201c3fb69b079b4da140dd5e66384e9a47f60
hexx.one <5312542+hexxone@users.noreply.github.com>
2023-03-11 23:13:30 +0100

    chore: CanvasTexture args

--- src/textures/CanvasTexture.ts
+++ src/textures/CanvasTexture.ts
@@ -1,3 +1,4 @@
+import { ClampToEdgeWrapping, LinearFilter, LinearMipmapLinearFilter, RGBAFormat, UnsignedByteType, LinearEncoding } from "../constants";
 import { Texture } from "./Texture";
 
 /**
@@ -5,15 +6,16 @@ import { Texture } from "./Texture";
  */
 class CanvasTexture extends Texture {
 	constructor(
-		canvas,
-		mapping,
-		wrapS,
-		wrapT,
-		magFilter,
-		minFilter,
-		format,
-		type,
-		anisotropy
+		canvas = Texture.DEFAULT_IMAGE,
+		mapping = Texture.DEFAULT_MAPPING,
+		wrapS = ClampToEdgeWrapping,
+		wrapT = ClampToEdgeWrapping,
+		magFilter = LinearFilter,
+		minFilter = LinearMipmapLinearFilter,
+		format = RGBAFormat,
+		type = UnsignedByteType,
+		anisotropy = 1,
+		encoding = LinearEncoding
 	) {
 		super(
 			canvas,
